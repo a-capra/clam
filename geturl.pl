3b31a526baa67dd22d91b576b98ad9f7a7829d0d
a-capra <andrea.capra85@gmail.com>
2019-10-10 11:05:19 -0700

    url validation/finder

---
+++ geturl.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w 
+
+# source code : http://urlregex.com/
+# test file: https://mathiasbynens.be/demo/url-regex
+
+#my $filename = './SHOULD_NOT_MATCH';
+my $filename = './SHOULD_MATCH';
+print("$filename\n");
+ 
+open(FH, '<', $filename) or die $!;
+
+my ($lines, $matches) = (0,0);
+ 
+while(<FH>)
+{
+    $lines++;
+    if( m{^(((ht|f)tp(s?))\://)?(www.|[a-zA-Z].)[a-zA-Z0-9\-\.]+\.(com|edu|gov|mil|net|org|biz|info|name|museum|us|ca|uk)(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\;\?\'\\\+&%\$#\=~_\-]+))*$} )
+    {
+        print("$_");
+	$matches++
+    }
+}
+ 
+close(FH);
+
+print("N of lines: $lines  N of matches: $matches\n")
